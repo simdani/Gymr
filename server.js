3eee1ef3c40f099cfd3c811f69768661d8950c57
Simonas Daniliauskas <dranxman@gmail.com>
2018-08-28 17:56:18 +0300

    prepare react for production

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('config');
+const path = require('path');
 const app = express();
 
 // connect with mongoose to mongodb
@@ -15,6 +16,14 @@ const gyms = require('./routes/api/gyms');
 let port;
 if (config.util.getEnv('NODE_ENV') !== 'test') {
     port = process.env.PORT || 5000;
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+    }
+
 } else {
     port = 5002;
 }
